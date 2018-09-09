1e2d12882f467741bcd183eef16c7a62ea363ee0
Marcin Wolinski <wolinski.m@gmail.com>
2018-09-09 21:14:46 +0200

    buttons

--- js/app.js
+++ js/app.js
@@ -10,5 +10,21 @@ document.addEventListener('DOMContentLoaded', function() {// tutaj będziemy umi
     });
     menuItem.addEventListener('mouseout', function() {
         dropdown.style.display = 'none';
-    })
+    });
+    var buttons = document.querySelectorAll(".read-more");
+
+    function showHide() {
+        var textArea = this.previousElementSibling;
+
+        if (textArea.style.display === 'none' || textArea.style.display === '') {
+            textArea.style.display = 'block';
+            this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+        } else {
+            textArea.style.display = 'none';
+            this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+        }
+    }
+    for (var i = 0; i <= buttons.length; i++) {
+        buttons[i].addEventListener('click', showHide);
+    }
 });
